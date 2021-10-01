dae40849fe12c43e53fc3092099f01a1a35ec824
cyclopicdog <jonathan_doig@hotmail.com>
2021-10-01 12:41:03 +0200

    he moves!'

--- src/js/Pacman.js
+++ src/js/Pacman.js
@@ -1,16 +1,39 @@
+const TILE_SIZE = 85;
+
 class Pacman {
-    constructor(xPos = 520.1000366210938, mouth = 'open', direction = 'left') {
-        (this.xPos = xPos), (this.mouth = mouth), (this.direction = direction);
+    constructor(xPos = 0, yPos = 0, mouth = 'open', direction = 'left') {
+        (this.xPos = xPos),
+            (this.yPos = yPos),
+            (this.mouth = mouth),
+            (this.direction = direction);
     }
 
     render() {
         this.pacman = this.elementFromHTML(
-            `<div class="entity entity--pac pacboy-active-light"></div>`
+            `<div class="entity entity--pac pacboy-active-light">`
         );
         document.addEventListener('keyup', (event) => {
             if (event.code === 'ArrowRight') {
                 this.move('right');
                 console.log('right');
+            }
+        });
+        document.addEventListener('keyup', (event) => {
+            if (event.code === 'ArrowLeft') {
+                this.move('left');
+                console.log('left');
+            }
+        });
+        document.addEventListener('keyup', (event) => {
+            if (event.code === 'ArrowUp') {
+                this.move('up');
+                console.log('up');
+            }
+        });
+        document.addEventListener('keyup', (event) => {
+            if (event.code === 'ArrowDown') {
+                this.move('down');
+                console.log('down');
             }
         });
     }
@@ -22,18 +45,27 @@ class Pacman {
     }
 
     update() {
-        this.pacman.style.left = `${this.xPos}px`;
-        console.log('updated');
-        console.log(this.xPos);
-        console.log(this.pacman);
-        console.log(this.pacman.style.left);
+        this.pacman.style.left = `${this.xPos * TILE_SIZE}px`;
+        this.pacman.style.top = `${this.yPos * TILE_SIZE}px`;
     }
 
     move(direction) {
         if (direction === 'right') {
-            this.xPos += 85;
+            this.xPos++;
         }
-        return this.mount();
+
+        if (direction === 'left') {
+            this.xPos--;
+        }
+
+        if (direction === 'up') {
+            this.yPos--;
+        }
+
+        if (direction === 'down') {
+            this.yPos++;
+        }
+        return this.update();
     }
 
     elementFromHTML(html, parent = 'div') {
